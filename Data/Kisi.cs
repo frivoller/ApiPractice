c49cb731f146476cad6765d2158e7306a9b0e620
Melis Yıldız <foxhes@outlook.com>
2024-12-12 18:30:45 +0300

    Add project files.

---
+++ Data/Kisi.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiPractice.Data
+{
+    public class Kisi
+    {
+        public int Id { get; set; }
+
+        public string Ad { get; set; } = "";
+    }
+}
---
+++ Data/Veri.cs
@@ -0,0 +1,18 @@
+﻿namespace ApiPractice.Data
+{
+    public static class Veri
+    {
+        public static List<Kisi> Kisiler()
+        {
+            return new List<Kisi>()
+            {
+                new Kisi() { Id = 1, Ad = "Mitski" },
+                new Kisi() { Id = 2, Ad = "Chappell Roan" },
+                new Kisi() { Id = 3, Ad = "Pedro Pascal" },
+                new Kisi() { Id = 4, Ad = "Paul Mescal" },
+                new Kisi() { Id = 5, Ad = "Keanu Reeves" },
+                new Kisi() { Id = 6, Ad = "Daisy Edgar-Jones" }
+            };
+        }
+    }
+}
